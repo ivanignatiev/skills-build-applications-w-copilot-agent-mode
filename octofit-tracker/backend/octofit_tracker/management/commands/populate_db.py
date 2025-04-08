a97f2d8cd3dc1862271bff39a76fbf40e43ffa5a
Ivan Ignatiev <ignatiev.ivan@gmail.com>
2025-04-08 09:42:41 +0000

    Add populate_db script and populate octofit_db with test data

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,73 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+from datetime import timedelta
+from bson import ObjectId
+
+class Command(BaseCommand):
+    help = 'Populate the database with test data for users, teams, activities, leaderboard, and workouts'
+
+    def handle(self, *args, **kwargs):
+        # Clear existing data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create users
+        users = [
+            User(_id=ObjectId(), username='thundergod', email='thundergod@octofit.edu', password='thundergodpassword'),
+            User(_id=ObjectId(), username='metalgeek', email='metalgeek@octofit.edu', password='metalgeekpassword'),
+            User(_id=ObjectId(), username='zerocool', email='zerocool@octofit.edu', password='zerocoolpassword'),
+            User(_id=ObjectId(), username='crashoverride', email='crashoverride@octofit.edu', password='crashoverridepassword'),
+            User(_id=ObjectId(), username='sleeptoken', email='sleeptoken@octofit.edu', password='sleeptokenpassword'),
+        ]
+        for user in users:
+            user.save()
+
+        # Create teams
+        blue_team = Team(_id=ObjectId(), name='Blue Team')
+        gold_team = Team(_id=ObjectId(), name='Gold Team')
+        blue_team.save()
+        gold_team.save()
+
+        # Assign users to teams
+        blue_team.members = users[:3]  # First three users
+        gold_team.members = users[3:]  # Last two users
+        blue_team.save()
+        gold_team.save()
+
+        # Create activities
+        activities = [
+            Activity(_id=ObjectId(), user=users[0], activity_type='Cycling', duration=timedelta(hours=1)),
+            Activity(_id=ObjectId(), user=users[1], activity_type='Crossfit', duration=timedelta(hours=2)),
+            Activity(_id=ObjectId(), user=users[2], activity_type='Running', duration=timedelta(hours=1, minutes=30)),
+            Activity(_id=ObjectId(), user=users[3], activity_type='Strength', duration=timedelta(minutes=30)),
+            Activity(_id=ObjectId(), user=users[4], activity_type='Swimming', duration=timedelta(hours=1, minutes=15)),
+        ]
+        for activity in activities:
+            activity.save()
+
+        # Create leaderboard entries
+        leaderboard_entries = [
+            Leaderboard(_id=ObjectId(), user=users[0], score=100),
+            Leaderboard(_id=ObjectId(), user=users[1], score=90),
+            Leaderboard(_id=ObjectId(), user=users[2], score=95),
+            Leaderboard(_id=ObjectId(), user=users[3], score=85),
+            Leaderboard(_id=ObjectId(), user=users[4], score=80),
+        ]
+        for entry in leaderboard_entries:
+            entry.save()
+
+        # Create workouts
+        workouts = [
+            Workout(_id=ObjectId(), name='Cycling Training', description='Training for a road cycling event'),
+            Workout(_id=ObjectId(), name='Crossfit', description='Training for a crossfit competition'),
+            Workout(_id=ObjectId(), name='Running Training', description='Training for a marathon'),
+            Workout(_id=ObjectId(), name='Strength Training', description='Training for strength'),
+            Workout(_id=ObjectId(), name='Swimming Training', description='Training for a swimming competition'),
+        ]
+        for workout in workouts:
+            workout.save()
+
+        self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data.'))
